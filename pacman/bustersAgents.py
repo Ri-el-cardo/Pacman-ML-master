f761ed8337bf3c5a67153ce7f4d19221a3ba9193
Mona LisaRi-el-cardodasasfgitMona <chaveztorresricardo@gmail.com>
2020-02-22 21:36:36 +0100

    pues le he puesto un 2

--- pacman/bustersAgents.py
+++ pacman/bustersAgents.py
@@ -10,7 +10,7 @@
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
-#aver si8 se actualiza esto
+#aver si8 se actualiza ecdsto 2
 
 import util
 from game import Agent
